dee86fb896ee21cd2c74d82d6a4a8dbd4b4ec64f
franqarroyo <a20160172@utem.edu.mx>
2018-05-14 11:07:28 -0500

    simon

--- scripts/menu.js
+++ scripts/menu.js
@@ -6,8 +6,24 @@ var sticky = menu1.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    menu1.classList.add("sticky")
+    menu1.style.fontSize="14px";
+    menu1.style.height="32px"; 
+    menu1.style.paddingBottom="30px";
+    /*
+    menu1.style.fontWeight="100";    
+    menu1.style.borderBottom="solid 2px gray";
+    document.getElementById("logo").style.width="130px";*/
+    menu1.classList.add("sticky");    
   } else {
     menu1.classList.remove("sticky");
+    menu1.style.fontSize="16px";
+    menu1.style.height="72px";
+    menu1.style.paddingBottom="0px";
+    /*
+    menu1.style.alignItems="center";
+    document.getElementById("pago-en-linea").style.paddingBottom="0px";
+    document.getElementById("menu1").style.borderBottom="solid 2px gray";
+    document.getElementById("menu1").style.paddingBottom="0px";
+    document.getElementById("logo").style.width="150px";*/
   }
 }
